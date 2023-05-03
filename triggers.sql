8a5dc7fa21f14ab49f89e9b7495b2347353f0bbd
RamunnoAJ <ramunnoagustin@gmail.com>
2023-05-03 13:59:22 -0300

    Agrega trigger para actualizar la titularidad de los jugadores lesionados

--- triggers.sql
+++ triggers.sql
@@ -23,4 +23,24 @@ BEGIN
         CLOSE cur;
     END IF;
 END$$
+
+CREATE TRIGGER tr_actualizar_titularidad_lesionado AFTER UPDATE ON jugadores
+FOR EACH ROW
+BEGIN 
+    DECLARE equipo_id INT;
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT s.id_equipo FROM seleccionados s WHERE s.id_jugador = NEW.id AND s.titular = 1;
+    DECLARE EXIT HANDLER FOR NOT FOUND SET done = 1;
+
+    IF !(NEW.lesionado <=> OLD.lesionado) AND NEW.lesionado = 1 THEN
+        OPEN cur;
+        WHILE NOT done DO
+            FETCH cur INTO equipo_id;
+            IF NOT done THEN
+                CALL cambiar_titularidad(NEW.id, equipo_id, 0);
+            END IF;
+        END WHILE;
+        CLOSE cur;
+    END IF;
+END$$
 DELIMITER ;
